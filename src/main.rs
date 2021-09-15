c6c2180c8edfde4ef2eb91ff9dfb07e5f4e37242
Yash Karandikar <nerdstep710@gmail.com>
2021-09-15 15:08:34 -0500

    Print Hello world to screen

--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,14 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop{}
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in b"Hello, World!".iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
 }
