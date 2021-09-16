bdb3dac4b759b8f958e8f7f57fe53902b55a7953
Yash Karandikar <nerdstep710@gmail.com>
2021-09-15 20:57:03 -0500

    Add structs for vga_buffer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
 use core::panic::PanicInfo;
+
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -10,14 +12,7 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in b"Hello, World!".iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
