cb33e987f13e33ffae15b6dd29fc4414d12e7b50
Yash Karandikar <nerdstep710@gmail.com>
2021-09-16 10:57:11 -0500

    Handle CPU exceptions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 #![no_std]
 #![no_main]
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+#![feature(abi_x86_interrupt)]
+
 mod vga_buffer;
+mod interrupts;
 use core::panic::PanicInfo;
 
 
@@ -11,12 +18,29 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
+fn init() {
+    interrupts::init_idt();
+}
+
 #[no_mangle]
 pub extern "C" fn _start() {
-    println!("The senate will decide your fate");
-    println!();
-    panic!("I am the senate");
+    println!("Hello, world");
 
+    init();
 
+    x86_64::instructions::interrupts::int3();
+
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
